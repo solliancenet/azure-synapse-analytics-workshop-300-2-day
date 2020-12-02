b43db9fc15ac747e93665b79c67f645d2a6ca5db
kylebunting <kyle.bunting@gmail.com>
2020-12-01 21:53:30 -0500

    Making script idempotent

--- artifacts/environment-setup/sql/03-create-schemas.sql
+++ artifacts/environment-setup/sql/03-create-schemas.sql
@@ -1,12 +1,23 @@
-create schema wwi
-go
-create schema wwi_security
-go
-create schema wwi_staging
-go
-create schema wwi_perf
-go
-create schema wwi_poc
-go
-create schema wwi_ml
-go
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = N'wwi')
+    EXEC('CREATE SCHEMA [wwi]')
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = N'wwi_security')
+    EXEC('CREATE SCHEMA [wwi_security]')
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = N'wwi_staging')
+    EXEC('CREATE SCHEMA [wwi_staging]')
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = N'wwi_perf')
+    EXEC('CREATE SCHEMA [wwi_perf]')
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = N'wwi_poc')
+    EXEC('CREATE SCHEMA [wwi_poc]')
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = N'wwi_ml')
+    EXEC('CREATE SCHEMA [wwi_ml]')
+GO
